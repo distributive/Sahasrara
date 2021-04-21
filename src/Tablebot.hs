6b23650fb19030fb920aaa977a1ed03cc23b774e
Finnbar Keating <finnjkeating@gmail.com>
2021-04-21 15:17:41 +0100

    Added Cron Jobs

--- src/Tablebot.hs
+++ src/Tablebot.hs
@@ -5,24 +5,33 @@ module Tablebot (
 ) where
 
 import Tablebot.Handler
-import Tablebot.Plugin (Plugin, combinePlugins, migrations)
+import Tablebot.Plugin (Plugin, combinePlugins, migrations, cronJobs)
 
 import Data.Text
 import Discord
 import qualified Data.Text.IO as TIO (putStrLn)
 import Database.Persist.Sqlite 
-import Control.Monad.Logger
+import Control.Monad.Logger (NoLoggingT(runNoLoggingT))
+import Control.Monad.IO.Class
+import Control.Concurrent
 
 runTablebot :: Text -> Text -> FilePath -> [Plugin] -> IO ()
 runTablebot dToken prefix dbpath plugins =
     let !plugin = combinePlugins plugins
     in do
+    -- Create multiple database threads.
     pool <- runNoLoggingT $ createSqlitePool (pack dbpath) 8
     -- TODO: this might have issues with duplicates?
     mapM_ (\migration -> runSqlPool (runMigration migration) pool) $ migrations plugin
+    -- Build list of cron jobs.
+    mvar <- newEmptyMVar :: IO (MVar [ThreadId])
+    -- Create a var to kill any ongoing tasks.
     userFacingError <- runDiscord $ def {
         discordToken = dToken,
         discordOnEvent =
-            flip runSqlPool pool . eventHandler plugin prefix
+            flip runSqlPool pool . eventHandler plugin prefix,
+        discordOnStart =
+            runSqlPool (mapM runCron (cronJobs plugin) >>= liftIO . putMVar mvar) pool,
+        discordOnEnd = takeMVar mvar >>= killCron
     }
     TIO.putStrLn userFacingError
--- src/Tablebot/Handler.hs
+++ src/Tablebot/Handler.hs
@@ -1,5 +1,5 @@
 module Tablebot.Handler (
-    eventHandler
+    eventHandler, runCron, killCron
 ) where
 
 import Tablebot.Plugin
@@ -10,9 +10,27 @@ import Discord
 import Discord.Types
 import Data.Text (Text)
 import Control.Monad (unless)
+import Control.Concurrent
+import Control.Monad.Trans.Reader
+import Control.Monad.Trans.Class
+import Control.Monad.IO.Class
 
 eventHandler :: Plugin -> Text -> Event -> DatabaseDiscord ()
 eventHandler pl prefix = \case
         MessageCreate m -> unless (userIsBot (messageAuthor m)) $
             parseCommands (commands pl) m prefix
         _ -> pure ()
+
+-- forkIO only works on direct IO, so we have to unwrap and rewrap the stack
+runCron :: CronJob -> DatabaseDiscord ThreadId
+runCron (CronJob delay fn) = do
+    db <- ask
+    discord <- lift ask
+    let unDB = runReaderT fn db
+    let unDiscord = runReaderT unDB discord
+    liftIO $ forkIO (loopWithDelay delay unDiscord)
+    where loopWithDelay :: Int -> IO () -> IO ()
+          loopWithDelay del fn = fn >> threadDelay del >> loopWithDelay del fn
+
+killCron :: [ThreadId] -> IO ()
+killCron = mapM_ killThread
