b8451860deb514c36326bfddcea5c48a941ccb16
L0neGamer <benjaminbwm@gmail.com>
2021-09-14 23:50:22 +0100

    renamed misc to basic on suggestion

--- src/Tablebot/Plugins/Basic.hs
+++ src/Tablebot/Plugins/Basic.hs
@@ -1,5 +1,5 @@
 -- |
--- Module      : Tablebot.Plugins.Misc
+-- Module      : Tablebot.Plugins.Basic
 -- Description : A very simple example plugin.
 -- Copyright   : (c) Finnbar Keating 2021
 -- License     : MIT
@@ -8,17 +8,17 @@
 -- Portability : POSIX
 --
 -- This is an example plugin which responds to vertain calls with specific responses.
-module Tablebot.Plugins.Misc (miscPlugin) where
+module Tablebot.Plugins.Basic (basicPlugin) where
 
 import Data.Text.Internal (Text)
 import Tablebot.Plugin.Discord (sendMessage)
 import Tablebot.Plugin.Parser (noArguments)
 import Tablebot.Plugin.Types (Command (Command), Plugin (commands), plug)
 
--- | The misc commands. Put the command in the first element of each tuple, and the 
+-- | The basic commands. Put the command in the first element of each tuple, and the 
 -- response in the second element of each tuple.
-miscCommands :: [(Text, Text)]
-miscCommands =
+basicCommands :: [(Text, Text)]
+basicCommands =
   [ ("pr", "You can make a pull request for that!"),
     ("benji", ":benji_sit:")
   ]
@@ -33,6 +33,6 @@ baseCommand a b =
         return ()
     )
 
--- | @miscPlugin@ assembles the call and response commands into a simple command list.
-miscPlugin :: Plugin
-miscPlugin = plug {commands = map (uncurry baseCommand) miscCommands}
+-- | @basicPlugin@ assembles the call and response commands into a simple command list.
+basicPlugin :: Plugin
+basicPlugin = plug {commands = map (uncurry baseCommand) basicCommands}
