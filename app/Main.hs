00a72c388ffb49a4fd674c48646ebaf950efaf29
distributive <github@ameliewd.com>
2021-09-20 12:04:25 +0100

    Added say command

--- app/Main.hs
+++ app/Main.hs
@@ -1,7 +1,7 @@
 module Main where
 
 import Tablebot
-import Tablebot.Plugins (flipPlugin, pingPlugin, quotePlugin, reminderPlugin, welcomePlugin)
+import Tablebot.Plugins (flipPlugin, pingPlugin, quotePlugin, reminderPlugin, sayPlugin, welcomePlugin)
 
 import LoadEnv (loadEnv)
 import System.Environment (getEnv, lookupEnv)
@@ -14,5 +14,5 @@ main = do
     dToken <- pack <$> getEnv "DISCORD_TOKEN"
     prefix <- pack . fromMaybe "!" <$> lookupEnv "PREFIX"
     dbpath <- getEnv "SQLITE_FILENAME"
-    let plugins = [flipPlugin, pingPlugin, quotePlugin, reminderPlugin, welcomePlugin]
+    let plugins = [flipPlugin, pingPlugin, quotePlugin, reminderPlugin, sayPlugin, welcomePlugin]
     runTablebot dToken prefix dbpath plugins
--- src/Tablebot/Plugins.hs
+++ src/Tablebot/Plugins.hs
@@ -12,11 +12,12 @@ to the Plugins directory, include an import here. This means that users only
 need to import @Tablebot.Plugins@ to import individual plugins.
 -}
 module Tablebot.Plugins (
-    flipPlugin, pingPlugin, quotePlugin, reminderPlugin, welcomePlugin
+    flipPlugin, pingPlugin, quotePlugin, reminderPlugin, sayPlugin, welcomePlugin
 ) where
 
 import Tablebot.Plugins.Flip (flipPlugin)
 import Tablebot.Plugins.Ping (pingPlugin)
 import Tablebot.Plugins.Quote (quotePlugin)
 import Tablebot.Plugins.Reminder (reminderPlugin)
+import Tablebot.Plugins.Say (sayPlugin)
 import Tablebot.Plugins.Welcome (welcomePlugin)
