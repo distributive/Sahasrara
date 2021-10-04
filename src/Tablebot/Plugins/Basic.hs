b249301d8d693413f447e371381ecfd67febb6e1
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-10-04 18:50:49 +0100

    Add help to the new commands

--- src/Tablebot/Plugins/Basic.hs
+++ src/Tablebot/Plugins/Basic.hs
@@ -13,7 +13,7 @@ module Tablebot.Plugins.Basic (basicPlugin) where
 import Data.Text.Internal (Text)
 import Tablebot.Plugin.Discord (sendMessage)
 import Tablebot.Plugin.Parser (noArguments)
-import Tablebot.Plugin.Types (Command (Command), Plugin (commands), plug)
+import Tablebot.Plugin.Types (Command (Command), Plugin (commands), plug, HelpPage(HelpPage), helpPages)
 
 -- | The basic commands. Put the command in the first element of each tuple, and the 
 -- response in the second element of each tuple.
@@ -33,6 +33,13 @@ baseCommand a b =
         return ()
     )
 
+prHelp :: HelpPage
+prHelp = HelpPage "pr" "you know what to do" "**PR**\nYou know what to do\n\n*Usage:* `pr`" [] 
+
+benjiHelp :: HelpPage
+benjiHelp = HelpPage "benji" "the almost mascot" "**Benji**\nThough he may sit, when put to test, the gender cube proved it was best\n\n*Usage:* `benji`" [] 
+
 -- | @basicPlugin@ assembles the call and response commands into a simple command list.
 basicPlugin :: Plugin
-basicPlugin = plug {commands = map (uncurry baseCommand) basicCommands}
+basicPlugin = plug {commands = map (uncurry baseCommand) basicCommands,
+                    helpPages = [prHelp, benjiHelp]}
