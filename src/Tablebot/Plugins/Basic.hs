b464769321c253138d30929be2082b9c547c9b6d
Thomas Bruce <44057980+the-Bruce@users.noreply.github.com>
2021-10-04 19:37:54 +0100

    Add some more structure to allow help text to be defined inline on basic commands

--- src/Tablebot/Plugins/Basic.hs
+++ src/Tablebot/Plugins/Basic.hs
@@ -1,4 +1,4 @@
--- |
+-- -- |
 -- Module      : Tablebot.Plugins.Basic
 -- Description : A very simple example plugin.
 -- Copyright   : (c) Finnbar Keating 2021
@@ -7,25 +7,36 @@
 -- Stability   : experimental
 -- Portability : POSIX
 --
--- This is an example plugin which responds to vertain calls with specific responses.
+-- This is an example plugin which responds to certain calls with specific responses.
 module Tablebot.Plugins.Basic (basicPlugin) where
 
 import Data.Text.Internal (Text)
+import Data.Text (toTitle)
 import Tablebot.Plugin.Discord (sendMessage)
 import Tablebot.Plugin.Parser (noArguments)
 import Tablebot.Plugin.Types (Command (Command), Plugin (commands), plug, HelpPage(HelpPage), helpPages)
 
--- | The basic commands. Put the command in the first element of each tuple, and the 
--- response in the second element of each tuple.
-basicCommands :: [(Text, Text)]
+-- * Some types to help clarify what's going on
+-- | @MiniHelpPage@ simplifies creating the help pages. You can either provide just the short and long help text and let 
+-- it autogenerate the formatting, or you can provide a full help page if you want more control
+data MiniHelpPage = Simple (Text, Text) | Advanced HelpPage
+-- | @BasicCommand@ is a tuple containing the important for this command.
+-- Put the command in the first element, the response in the second element of each tuple, 
+-- and a 'MiniHelpPage' for your command in the third
+type BasicCommand = (Text, Text, MiniHelpPage)
+
+-- | The basic commands.
+basicCommands :: [BasicCommand]
 basicCommands =
-  [ ("pr", "You can make a pull request for that!"),
-    ("benji", ":benji_sit:")
+  [ ("pr", "You can make a pull request for that!", 
+      Simple ("you know what to do", "You know what to do")),
+    ("benji", ":benji_sit:", 
+      Simple ("the almost mascot", "Though he may sit, when put to test, the gender cube proved it was best"))
   ]
 
 -- | Given command text "a", reply with text "b".
-baseCommand :: Text -> Text -> Command
-baseCommand a b =
+baseCommand :: BasicCommand -> Command
+baseCommand (a, b, _) =
   Command
     a
     ( noArguments $ \m -> do
@@ -33,13 +44,12 @@ baseCommand a b =
         return ()
     )
 
-prHelp :: HelpPage
-prHelp = HelpPage "pr" "you know what to do" "**PR**\nYou know what to do\n\n*Usage:* `pr`" [] 
+baseHelp :: BasicCommand -> HelpPage
+baseHelp (_, _, Advanced help) = help
+baseHelp (a, _, Simple (short, long)) = HelpPage a short ("**"<>toTitle a<>"**\n"<>long<>"\n\n*Usage:* `"<>a<>"`") []
 
-benjiHelp :: HelpPage
-benjiHelp = HelpPage "benji" "the almost mascot" "**Benji**\nThough he may sit, when put to test, the gender cube proved it was best\n\n*Usage:* `benji`" [] 
 
 -- | @basicPlugin@ assembles the call and response commands into a simple command list.
 basicPlugin :: Plugin
-basicPlugin = plug {commands = map (uncurry baseCommand) basicCommands,
-                    helpPages = [prHelp, benjiHelp]}
+basicPlugin = plug {commands = map baseCommand basicCommands,
+                    helpPages = map baseHelp basicCommands}
