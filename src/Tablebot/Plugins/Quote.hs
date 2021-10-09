15c7452f7a9b74d1845ee0ad90ce2bc9f3325ffb
Finnbar Keating <finnjkeating@gmail.com>
2021-10-09 11:47:01 +0100

    ORMOLO HATH DECREED THIS

--- src/Tablebot/Plugins/Quote.hs
+++ src/Tablebot/Plugins/Quote.hs
@@ -1,33 +1,34 @@
-{-|
-Module      : Tablebot.Plugins.Quote
-Description : A more complex example using databases.
-Copyright   : (c) Finnbar Keating 2021
-License     : MIT
-Maintainer  : finnjkeating@gmail.com
-Stability   : experimental
-Portability : POSIX
-
-This is an example plugin which allows user to @!quote add@ their favourite
-quotes and then @!quote show n@ a particular quote.
--}
-module Tablebot.Plugins.Quote (
-    quotePlugin
-) where
-
-import Tablebot.Plugin
-import Tablebot.Plugin.Parser (quoted, sp, untilEnd, number)
-import Tablebot.Plugin.Discord (sendMessageVoid, Message)
+-- |
+-- Module      : Tablebot.Plugins.Quote
+-- Description : A more complex example using databases.
+-- Copyright   : (c) Finnbar Keating 2021
+-- License     : MIT
+-- Maintainer  : finnjkeating@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
+--
+-- This is an example plugin which allows user to @!quote add@ their favourite
+-- quotes and then @!quote show n@ a particular quote.
+module Tablebot.Plugins.Quote
+  ( quotePlugin,
+  )
+where
 
 import Data.Text (append, pack)
-import Text.Megaparsec
 import Database.Persist
 import Database.Persist.Sqlite
 import Database.Persist.TH
 import GHC.Int (Int64)
+import Tablebot.Plugin
+import Tablebot.Plugin.Discord (Message, sendMessageVoid)
+import Tablebot.Plugin.Parser (number, quoted, sp, untilEnd)
+import Text.Megaparsec
 
 -- Our Quote table in the database. This is fairly standard for Persistent,
 -- however you should note the name of the migration made.
-share [mkPersist sqlSettings, mkMigrate "quoteMigration"] [persistLowerCase|
+share
+  [mkPersist sqlSettings, mkMigrate "quoteMigration"]
+  [persistLowerCase|
 Quote
     quote String
     author String
@@ -36,50 +37,55 @@ Quote
 
 -- | Our quote command, which combines @addQuote@ and @showQuote@.
 quote :: Command
-quote = Command "quote" (
-    ((try (chunk "add") *> addQuote) <|> (try (chunk "show") *> showQuote))
-        <?> "Unknown quote functionality.")
+quote =
+  Command
+    "quote"
+    ( ((try (chunk "add") *> addQuote) <|> (try (chunk "show") *> showQuote))
+        <?> "Unknown quote functionality."
+    )
 
 -- | @addQuote@, which looks for a message of the form
 -- @!quote add "quoted text" - author@, and then stores said quote in the
 -- database, returning the ID used.
 addQuote :: Parser (Message -> DatabaseDiscord ())
 addQuote = do
-    sp 
-    quote <- try quoted <?> error ++ " (needed a quote)"
-    sp
-    single '-' <?> error ++ " (needed hyphen)"
-    sp
-    addQ quote <$> untilEnd <?> error ++ " (needed author)"
-  where addQ :: String -> String -> Message -> DatabaseDiscord ()
-        addQ quote author m = do
-            added <- insert $ Quote quote author
-            let res = pack $ show $ fromSqlKey added
-            sendMessageVoid m ("Quote added as #" `append` res)
-        error = "Syntax is: .quote add \"quote\" - author"
+  sp
+  quote <- try quoted <?> error ++ " (needed a quote)"
+  sp
+  single '-' <?> error ++ " (needed hyphen)"
+  sp
+  addQ quote <$> untilEnd <?> error ++ " (needed author)"
+  where
+    addQ :: String -> String -> Message -> DatabaseDiscord ()
+    addQ quote author m = do
+      added <- insert $ Quote quote author
+      let res = pack $ show $ fromSqlKey added
+      sendMessageVoid m ("Quote added as #" `append` res)
+    error = "Syntax is: .quote add \"quote\" - author"
 
 -- | @showQuote@, which looks for a message of the form @!quote show n@, looks
 -- that quote up in the database and responds with that quote.
 showQuote :: Parser (Message -> DatabaseDiscord ())
 showQuote = do
-    sp
-    num <- number <?> error
-    let id = fromIntegral num :: Int64
-    return $ showQ id
-  where showQ :: Int64 -> Message -> DatabaseDiscord ()
-        showQ id m = do
-            qu <- get $ toSqlKey id
-            case qu of
-                Just (Quote txt author) ->
-                    sendMessageVoid m $ pack $ txt ++ " - " ++ author
-                Nothing -> sendMessageVoid m "Couldn't get that quote!"
-        error = "Syntax is: .quote show n"
+  sp
+  num <- number <?> error
+  let id = fromIntegral num :: Int64
+  return $ showQ id
+  where
+    showQ :: Int64 -> Message -> DatabaseDiscord ()
+    showQ id m = do
+      qu <- get $ toSqlKey id
+      case qu of
+        Just (Quote txt author) ->
+          sendMessageVoid m $ pack $ txt ++ " - " ++ author
+        Nothing -> sendMessageVoid m "Couldn't get that quote!"
+    error = "Syntax is: .quote show n"
 
 showQuoteHelp :: HelpPage
-showQuoteHelp = HelpPage "show" "show a quote by number" "**Show Quote**\nShows a quote by id\n\n*Usage:* `quote show <id>`" [] 
+showQuoteHelp = HelpPage "show" "show a quote by number" "**Show Quote**\nShows a quote by id\n\n*Usage:* `quote show <id>`" []
 
 addQuoteHelp :: HelpPage
-addQuoteHelp = HelpPage "add" "add a new quote" "**Add Quote**\nAdds a quote\n\n*Usage:* `quote add \"quote\" - author`" [] 
+addQuoteHelp = HelpPage "add" "add a new quote" "**Add Quote**\nAdds a quote\n\n*Usage:* `quote add \"quote\" - author`" []
 
 quoteHelp :: HelpPage
 quoteHelp = HelpPage "quote" "store and retrieve quotes" "**Quotes**\nAllows storing and retrieving quotes" [showQuoteHelp, addQuoteHelp]
@@ -87,4 +93,4 @@ quoteHelp = HelpPage "quote" "store and retrieve quotes" "**Quotes**\nAllows sto
 -- | @quotePlugin@ assembles the @quote@ command (consisting of @add@ and
 -- @show@) and the database migration into a plugin.
 quotePlugin :: Plugin
-quotePlugin = plug { commands = [quote], migrations = [quoteMigration], helpPages=[quoteHelp] }
+quotePlugin = plug {commands = [quote], migrations = [quoteMigration], helpPages = [quoteHelp]}
--- src/Tablebot/Plugins/Reminder.hs
+++ src/Tablebot/Plugins/Reminder.hs
@@ -1,45 +1,55 @@
+{-# LANGUAGE ImportQualifiedPost #-}
 {-# LANGUAGE QuasiQuotes #-}
-{-|
-Module      : Tablebot.Plugins.Quote
-Description : A complex example using databases and cron jobs.
-Copyright   : (c) Finnbar Keating 2021
-License     : MIT
-Maintainer  : finnjkeating@gmail.com
-Stability   : experimental
-Portability : POSIX
 
-This is an example plugin which allows user to ask the bot to remind them about
-something later in time.
--}
-module Tablebot.Plugins.Reminder (
-    reminderPlugin
-) where
+-- |
+-- Module      : Tablebot.Plugins.Quote
+-- Description : A complex example using databases and cron jobs.
+-- Copyright   : (c) Finnbar Keating 2021
+-- License     : MIT
+-- Maintainer  : finnjkeating@gmail.com
+-- Stability   : experimental
+-- Portability : POSIX
+--
+-- This is an example plugin which allows user to ask the bot to remind them about
+-- something later in time.
+module Tablebot.Plugins.Reminder
+  ( reminderPlugin,
+  )
+where
 
-import Tablebot.Plugin
-import Tablebot.Plugin.Parser (number, quoted, space)
-import Tablebot.Plugin.Discord
-    (Message, getMessage, sendMessageVoid)
-
-import Discord.Types
-import Data.Word (Word64)
+import Control.Monad (forM_, when)
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import Data.Text (append, pack)
+import Data.Time.Calendar.MonthDay
+  ( monthAndDayToDayOfYear,
+    monthLength,
+  )
+import Data.Time.Calendar.OrdinalDate
+  ( fromOrdinalDate,
+    isLeapYear,
+  )
 import Data.Time.Clock (secondsToDiffTime)
 import Data.Time.Clock.System (getSystemTime, systemToUTCTime)
-import Data.Time.Calendar.OrdinalDate
-    (isLeapYear, fromOrdinalDate)
-import Data.Time.Calendar.MonthDay
-    (monthAndDayToDayOfYear, monthLength)
-import Text.Megaparsec
+import Data.Word (Word64)
 import Database.Esqueleto
 import Database.Persist qualified as P (delete)
 import Database.Persist.TH
-import Control.Monad (when, forM_)
-import Control.Monad.IO.Class (MonadIO(liftIO))
-import Data.Text (pack, append)
+import Discord.Types
+import Tablebot.Plugin
+import Tablebot.Plugin.Discord
+  ( Message,
+    getMessage,
+    sendMessageVoid,
+  )
+import Tablebot.Plugin.Parser (number, quoted, space)
+import Text.Megaparsec
 import Text.RawString.QQ
 
 -- Our Reminder table in the database. This is fairly standard for Persistent,
 -- however you should note the name of the migration made.
-share [mkPersist sqlSettings, mkMigrate "reminderMigration"] [persistLowerCase|
+share
+  [mkPersist sqlSettings, mkMigrate "reminderMigration"]
+  [persistLowerCase|
 Reminder
     reminderCid Word64
     reminderMid Word64
@@ -53,44 +63,44 @@ Reminder
 dateTimeParser :: Parser UTCTime
 -- TODO: better parsing.
 dateTimeParser = do
-    day <- number
-    single '/'
-    month <- number
-    when (month > 12) $ fail "There are only twelve months in a year!"
-    single '/'
-    year <- number
-    let leapYear = isLeapYear (toInteger year)
-    when (day > monthLength leapYear month) $ fail "That month doesn't have enough days!"
-    space
-    hour <- number
-    single ':'
-    minute <- number
-    when (hour >= 24) $ fail "There are only 24 hours in a day!"
-    when (minute >= 60) $ fail "There are only sixty minutes in an hour!"
-    let monthday = monthAndDayToDayOfYear leapYear month day
-    let yearday = fromOrdinalDate (toInteger year) monthday
-    let difftime = secondsToDiffTime $ toInteger $ (minute * 60) + (hour * 60 * 60)
-    return $ UTCTime yearday difftime
+  day <- number
+  single '/'
+  month <- number
+  when (month > 12) $ fail "There are only twelve months in a year!"
+  single '/'
+  year <- number
+  let leapYear = isLeapYear (toInteger year)
+  when (day > monthLength leapYear month) $ fail "That month doesn't have enough days!"
+  space
+  hour <- number
+  single ':'
+  minute <- number
+  when (hour >= 24) $ fail "There are only 24 hours in a day!"
+  when (minute >= 60) $ fail "There are only sixty minutes in an hour!"
+  let monthday = monthAndDayToDayOfYear leapYear month day
+  let yearday = fromOrdinalDate (toInteger year) monthday
+  let difftime = secondsToDiffTime $ toInteger $ (minute * 60) + (hour * 60 * 60)
+  return $ UTCTime yearday difftime
 
 -- @reminderParser@ parses a reminder request of the form
 -- @!remind "reminder" at time@.
 reminderParser :: Parser (Message -> DatabaseDiscord ())
 reminderParser = do
-    content <- quoted
-    chunk " at "
-    time <- dateTimeParser
-    return $ addReminder time content
+  content <- quoted
+  chunk " at "
+  time <- dateTimeParser
+  return $ addReminder time content
 
 -- @addReminder@ takes a @time@ to remind at and the @content@ of a reminder
 -- and adds a reminder at that time. Note that this is all done in UTC, so
 -- currently ignores the user's timezone... (TODO fix)
 addReminder :: UTCTime -> String -> Message -> DatabaseDiscord ()
 addReminder time content m = do
-    let (Snowflake cid) = messageChannel m
-        (Snowflake mid) = messageId m
-    added <- insert $ Reminder cid mid time content
-    let res = pack $ show $ fromSqlKey added
-    sendMessageVoid m ("Reminder added as #" `append` res)
+  let (Snowflake cid) = messageChannel m
+      (Snowflake mid) = messageId m
+  added <- insert $ Reminder cid mid time content
+  let res = pack $ show $ fromSqlKey added
+  sendMessageVoid m ("Reminder added as #" `append` res)
 
 -- | @reminderCommand@ is a command implementing the functionality in
 -- @reminderParser@ and @addReminder@.
@@ -102,38 +112,45 @@ reminderCommand = Command "remind" reminderParser
 -- message originally triggering it in the database.
 reminderCron :: DatabaseDiscord ()
 reminderCron = do
-    now <- liftIO $ systemToUTCTime <$> getSystemTime
-    liftIO $ putStrLn $ "running reminder cron at " ++ show now
-    entitydue <- select $
-        from $ \r -> do
-            where_ (r ^. ReminderTime <=. val now)
-            return r
-    liftIO $ mapM_ (print . entityVal) entitydue
-    forM_ entitydue $ \r ->
-        let (Reminder cid mid time content) = entityVal r
-        in do
-            res <- getMessage (Snowflake cid) (Snowflake mid)
-            case res of
-                Left e -> pure ()
-                Right mess -> do
-                    let (Snowflake uid) = userId (messageAuthor mess)
-                    sendMessageVoid mess $ pack $
-                        "Reminder to <@" ++ show uid ++ ">! " ++ content
-                    P.delete (entityKey r)
+  now <- liftIO $ systemToUTCTime <$> getSystemTime
+  liftIO $ putStrLn $ "running reminder cron at " ++ show now
+  entitydue <- select $
+    from $ \r -> do
+      where_ (r ^. ReminderTime <=. val now)
+      return r
+  liftIO $ mapM_ (print . entityVal) entitydue
+  forM_ entitydue $ \r ->
+    let (Reminder cid mid time content) = entityVal r
+     in do
+          res <- getMessage (Snowflake cid) (Snowflake mid)
+          case res of
+            Left e -> pure ()
+            Right mess -> do
+              let (Snowflake uid) = userId (messageAuthor mess)
+              sendMessageVoid mess $
+                pack $
+                  "Reminder to <@" ++ show uid ++ ">! " ++ content
+              P.delete (entityKey r)
 
 reminderHelp :: HelpPage
-reminderHelp = HelpPage "remind" "ask the bot to remind you to do things in the future" [r|**Reminders**
+reminderHelp =
+  HelpPage
+    "remind"
+    "ask the bot to remind you to do things in the future"
+    [r|**Reminders**
 Send a reminder to yourself or others. Pick a date and time, and the tablebot will poke you to remember at your preordained moment.
 
-*Usage:* `remind "reminder" at <time>`|] []
+*Usage:* `remind "reminder" at <time>`|]
+    []
 
 -- | @reminderPlugin@ builds a plugin providing reminder asking functionality
 -- (@reminderCommand@), reminding functionality (via the cron job specified by
 -- @reminderCron@) and the database information.
 reminderPlugin :: Plugin
-reminderPlugin = plug {
-    commands = [reminderCommand],
-    cronJobs = [CronJob 60000000 reminderCron],
-    migrations = [reminderMigration],
-    helpPages = [reminderHelp]
-}
+reminderPlugin =
+  plug
+    { commands = [reminderCommand],
+      cronJobs = [CronJob 60000000 reminderCron],
+      migrations = [reminderMigration],
+      helpPages = [reminderHelp]
+    }
