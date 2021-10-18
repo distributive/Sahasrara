fc6f395566dbb3f6b0a6c52117c1b8376296e16d
L0neGamer <benjaminbwm@gmail.com>
2021-10-18 15:45:02 +0100

    ormolu bargains with no one

--- src/Tablebot/Plugins/Quote.hs
+++ src/Tablebot/Plugins/Quote.hs
@@ -23,7 +23,7 @@ import Tablebot.Plugin
 import Tablebot.Plugin.Discord (Message, sendMessageVoid)
 import Tablebot.Plugin.Permission (requirePermission)
 import Tablebot.Plugin.SmartCommand
-import Text.RawString.QQ ( r )
+import Text.RawString.QQ (r)
 
 -- Our Quote table in the database. This is fairly standard for Persistent,
 -- however you should note the name of the migration made.
