a7c08a4bd3b802c81a966099ccfe47d381803352
L0neGamer <benjaminbwm@gmail.com>
2021-12-06 09:47:35 +0000

    added issue, modified benji, gave a simple about

--- src/Tablebot/Plugins/Basic.hs
+++ src/Tablebot/Plugins/Basic.hs
@@ -43,9 +43,17 @@ basicCommands =
       "You can make a pull request for that!",
       Simple ("you know what to do", "You know what to do")
     ),
+    ( "issue",
+      "You can submit an issue for that!",
+      Simple ("you know what you want someone else to do", "You know what you want someone else to do")
+    ),
     ( "benji",
-      ":benji_sit:",
+      "<:benji_sit:686131468560367627>",
       Simple ("the almost mascot", "Though he may sit, when put to test, the gender cube proved it was best")
+    ),
+    ( "about",
+      "This bot was created by finnbar to replace a couple of other bots in Tabletop. It's written in Haskell, and you can find the github here: <https://github.com/WarwickTabletop/tablebot>. There are setup guides and a contributor's guide to help you get started.",
+      Simple ("you know what you want someone else to do", "You know what you want someone else to do")
     )
   ]
 
