89ac22daafa26b8b85da7ed2e230f22e46e606a3
distributive <github@ameliewd.com>
2022-03-27 13:29:29 +0100

    Removed arrows from help text

--- src/Sahasrara/Plugins/Netrunner/Command/Help.hs
+++ src/Sahasrara/Plugins/Netrunner/Command/Help.hs
@@ -36,9 +36,9 @@ The following fields are not implemented:
 > `z` - rotation
 
 **Usage**
-`search x:advanced ` -> all cards containing the text "advanced"
-`search o:1 f:nbn" ` -> all 1-cost cards in NBN
-`search a:"and the"` -> all cards with "and the" in their flavour text|]
+`search x:advanced ` all cards containing the text "advanced"
+`search o:1 f:nbn" ` all 1-cost cards in NBN
+`search a:"and the"` all cards with "and the" in their flavour text|]
     []
     None
 
@@ -52,16 +52,16 @@ customHelp =
 The order of card parameters does not matter, and if you mispell a card parameter (e.g. "typ" instead of "type") it will attempt to correct it.
 
 **Usage**
-`custom type:agenda                ` -> creates an agenda
-`custom title:"Name" text:"Lorem." ` -> creates a card with a title and text
-`custom faction:"nbn"              ` -> creates a card with a faction
-`custom keywords:"AP - Hardware"   ` -> creates a card with subtypes
-`custom advancement:5 points:3     ` -> creates a card with an advancement requirement and agenda points
-`custom cost:3 trash:2             ` -> creates a card with play/rez cost and trash cost
-`custom strength:4                 ` -> creates a card with strength
-`custom minSize:40 maxInf:15 link:2` -> creates a card with a minimum deck size, maximum influence, and link
-`custom flavour:"Raspberry & mint" ` -> creates a card with flavour text
-`custom unique:true                ` -> creates a unique card|]
+`custom type:agenda                ` creates an agenda
+`custom title:"Name" text:"Lorem." ` creates a card with a title and text
+`custom faction:"nbn"              ` creates a card with a faction
+`custom keywords:"AP - Hardware"   ` creates a card with subtypes
+`custom advancement:5 points:3     ` creates a card with an advancement requirement and agenda points
+`custom cost:3 trash:2             ` creates a card with play/rez cost and trash cost
+`custom strength:4                 ` creates a card with strength
+`custom minSize:40 maxInf:15 link:2` creates a card with a minimum deck size, maximum influence, and link
+`custom flavour:"Raspberry & mint" ` creates a card with flavour text
+`custom unique:true                ` creates a unique card|]
     []
     None
 
@@ -75,7 +75,7 @@ banListHelp =
 `latest` and `active` will provide their respective banlists (they differ only when the latest banlist has not yet been made active). If no argument is given it will instead list all banlists from Netrunner history.
 
 **Usage**
-`banlist name` -> displays the history of the banlist version matching "name"|]
+`banlist name` displays the history of the banlist version matching "name"|]
     []
     None
 
@@ -88,11 +88,11 @@ rulesHelp =
     [r|Provides the official rulings on certain aspects of the game. Rulings are from the NISEI Comprehensive Rules v1.5.
 
 **Usage**
-`rules access` -> Shows the steps of accessing a card
-`rules breach` -> Shows the steps of breaching a server
-`rules run` -> Shows the timing structure of a run
-`rules turn` -> Shows the timing structure of a turn
-`rules corp` -> Shows the timing structure of a Corp's turn
-`rules runner` -> Shows the timing structure of a Runner's turn|]
+`rules access` shows the steps of accessing a card
+`rules breach` shows the steps of breaching a server
+`rules run` shows the timing structure of a run
+`rules turn` shows the timing structure of a turn
+`rules corp` shows the timing structure of a Corp's turn
+`rules runner` shows the timing structure of a Runner's turn|]
     []
     None
