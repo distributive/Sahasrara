061e79007700032ffdb5cdbb763ffedb88b4a06a
distributive <github@ameliewd.com>
2022-05-10 11:53:51 +0100

    Condensed code

--- src/Sahasrara/Plugins/Netrunner/Utility/Find.hs
+++ src/Sahasrara/Plugins/Netrunner/Utility/Find.hs
@@ -11,10 +11,8 @@
 -- Functions for finding cards in the api.
 module Sahasrara.Plugins.Netrunner.Utility.Find (queryCard) where
 
-import Data.Bifunctor (first)
 import Data.Maybe (fromMaybe)
 import Data.Text (Text, isInfixOf, unpack)
-import qualified Data.Text as T
 import Sahasrara.Plugins.Netrunner.Type.Card as Card (Card (..))
 import Sahasrara.Plugins.Netrunner.Type.NrApi (NrApi (..))
 import Sahasrara.Plugins.Netrunner.Utility.Alias (fromAlias)
@@ -27,34 +25,17 @@ import Sahasrara.Utility.Utils (standardise)
 -- the cards if no cards are found
 -- If the given query matches an alias, it will first dereference that alias
 queryCard :: NrApi -> Text -> Card
-queryCard NrApi {cards = cards, cardAliases = cardAliases} txt =
-  let q = fromAlias cardAliases $ standardise txt
-   in findCard (substringSearch pairs q) q pairs
+queryCard NrApi {cards = cards, cardAliases = cardAliases} query =
+  let q = fromAlias cardAliases $ standardise query
+   in case filter (isInfixOf (standardise q) . fst) pairs of
+        [] -> fuzzyQueryCard pairs query
+        [res] -> snd res
+        res -> fuzzyQueryCard res query
   where
     pairs = zip (map (standardise . fromMaybe "" . Card.title) cards) cards
-    substringSearch pairs' searchTxt =
-      let pres = filter (isPrefixOf searchTxt . fst) pairs'
-          subs = filter (isInfixOf searchTxt . fst) pairs'
-       in case length pres of
-            0 -> subs
-            _ -> pres
-    isPrefixOf :: Text -> Text -> Bool
-    isPrefixOf short long = short == T.take (T.length short) long
-
--- | @findCard@ finds a card from the given list of pairs that is some subset of a
--- full list. If the sublist is empty, it will fuzzy search the full list. If the sublist
--- has exactly 1 element, it'll return that element. If the sublist has multiple
--- elements, it will fuzzy search the sublist
-findCard :: [(Text, Card)] -> Text -> [(Text, Card)] -> Card
-findCard [] searchTxt allCards = fuzzyQueryCard allCards searchTxt
-findCard [(_, card)] _ _ = card
-findCard cards searchTxt _ = fuzzyQueryCard cards searchTxt
-
--- | @queryCard@ fuzzy searches the given library of cards by title.
-fuzzyQueryCard :: [(Text, Card)] -> Text -> Card
-fuzzyQueryCard pairs = closestValueWithCosts editCosts unpackedPairs . unpack
-  where
-    unpackedPairs = fmap (first unpack) pairs
+    fuzzyQueryCard pairs' =
+      let unpackedPairs = fmap (\(x, y) -> (unpack x, y)) pairs'
+       in closestValueWithCosts editCosts unpackedPairs . unpack
     editCosts =
       FuzzyCosts
         { deletion = 10,
