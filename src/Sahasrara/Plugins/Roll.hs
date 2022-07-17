a0470d5dd102f5a6138aaf17948f9ae2678a0011
L0neGamer <benjaminbwm@gmail.com>
2022-07-17 18:36:29 +0100

    adding basic dice stuff

---
+++ src/Sahasrara/Plugins/Roll.hs
@@ -0,0 +1,6 @@
+module Sahasrara.Plugins.Roll (rollPlugin) where
+
+import Sahasrara.Plugins.Roll.Plugin (rollPlugin)
+
+-- hi
+-- if you have any questions about this, go to the `tablebot` project and find Benji, or L0neGamer
